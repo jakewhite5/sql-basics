2418e26800af571f092775aa87001cff619828db
jakewhite5 <jakewhite5@yahoo.com>
2021-06-23 11:41:20 -0700

    basics complete

--- create_schema.sql
+++ create_schema.sql
@@ -1,9 +1,37 @@
 -- Schema
-DROP TABLE IF EXISTS students;
+DROP TABLE IF EXISTS students CASCADE;
+DROP TABLE IF EXISTS addresses CASCADE;
+DROP TABLE IF EXISTS classes CASCADE;
+DROP TABLE IF EXISTS enrollments CASCADE;
 CREATE TABLE students (
-  id           serial PRIMARY KEY,
+  id   serial PRIMARY KEY,
   first_name   varchar(255) NOT NULL,
   last_name    varchar(255) NOT NULL,
   birthdate    date NOT NULL,
   address_id   integer
+);
+
+CREATE TABLE addresses (
+  id           serial PRIMARY KEY,
+  line_1       varchar(255) NOT NULL,
+  city         varchar(64),
+  state        varchar(64),
+  zipcode      integer
+);
+
+CREATE TABLE classes (
+  id serial Primary Key,
+  name varchar(64),
+  credits integer
+);
+
+Create table enrollments (
+  id serial primary key,
+  student_id integer,
+  class_id integer,
+  grade varchar(6),
+    FOREIGN KEY(student_id)
+      REFERENCES students(id),
+    FOREIGN KEY(class_id)
+    REFERENCES classes(id)
 );
